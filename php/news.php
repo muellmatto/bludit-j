c8e93d1e32d6142884de1ff944e5ead8a473d2bd
matto <matto@damniam.de>
2017-04-06 10:40:52 +0200

    mixed list of posts of two tags

--- php/news.php
+++ php/news.php
@@ -1,57 +1,34 @@
 <?php
             echo $plugins['all']['treadmill']->get();
+
             // get all posts
             $totalPublishedPosts = $dbPosts->numberPost(true);
             $posts = buildPostsForPage(0, $totalPublishedPosts, true, false);
-            // build 2 arrays of filtered posts
-            $exhiPosts = [];
-            $newsPosts = [];
+            
+            // build a list of filtered posts
+            $newsExhiPosts = [];
             foreach ($posts as $Post) {
-                if (strpos($Post->tags(), 'Exhi') !== false) {
-                    $exhiPosts[] = $Post;
-                }
-                if (strpos($Post->tags(), 'News') !== false) {
-                    $newsPosts[] = $Post;
+                if ( (strpos($Post->tags(), 'Exhi') !== false) || (strpos($Post->tags(), 'News') !== false)) {
+                    $newsExhiPosts[] = $Post;
                 }
             }
-            // now we have two arrays, one with news, one with exhibitions
-
-            // push newspage content into page
-            // echo '<div class="leftCol">';
-            // echo $content;
-            // fill in news posts
-            foreach ($newsPosts as $n) {
-                echo '<div class="workslist news">';
-                echo '<a href="'.$n->permalink() .'">';
-                echo '<h3>'.$n->title().'</h3>';
-                echo '<h5>'.$n->date().'</h5>';
-                if($n->coverImage()) {
-                    echo '<img src="'.$n->coverImage().'" alt="Cover Image">';
-                }
-                echo '</a>';
-                echo '</div>';
-            }
-            // echo '</div>';
-
-            // fill site with exhibitions
-            // echo '<div class="rightCol">';
-            foreach ($exhiPosts as $e) {
-                echo '<div class="workslist exhibitions">';
-                echo '<a href="'.$e->permalink() .'">';
-                echo '<h3>'.$e->title().'</h3>';
-                echo '<h5>'.$e->date().'</h5>';
-                if($Post->coverImage()) {
-                    echo '<img src="'.$e->coverImage().'" alt="Cover Image">';
+            
+            // now we have a filtered list with Posts of both tags
+            foreach ($newsExhiPosts as $i) {
+                if ( strpos($i->tags(), 'News') !== false ) {
+                    $class = "news";
                 } else {
-                    echo '<h4>'.$e->title().'</h4>';
+                    $class = "exhibitions";
+                }
+                echo '<div class="workslist '.$class.'">';
+                echo '<a href="'.$i->permalink() .'">';
+                echo '<h3>'.$i->title().'</h3>';
+                echo '<h5>'.$i->date().'</h5>';
+                if($i->coverImage()) {
+                    echo '<img src="'.$i->coverImage().'" alt="Cover Image">';
                 }
                 echo '</a>';
                 echo '</div>';
             }
-            // echo '</div>';
-
-
-
-                    echo '<div style="clear: both;"></div>';
             echo '<div style="clear: both;"></div>';
 ?>
